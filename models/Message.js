cd1237856257b9cf65b2c33e3378b1f3812386f2
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-03-27 20:07:33 -0500

    adds message schema/model

---
+++ models/Message.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema(
+	{
+		body: {
+			type: String,
+			required: true,
+			trim: true,
+			minLength: 1,
+		},
+		user: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'User',
+				required: true,
+			},
+		],
+		type: {
+			group: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Group',
+			},
+			chat: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Chat',
+			},
+		},
+	},
+	{ timestamps: true }
+);
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
