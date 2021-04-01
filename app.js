e391f2c355c868559439fdf42f4a01ba42e5fd0f
mrdanishjr1992 <mrdanishjr1992@gmail.com>
2021-03-31 20:52:49 -0500

    altered delete route for user

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 4000;
 const routes = require('./routes');
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use((req, res, next) => {
-	next();
-});
+// app.use((req, res, next) => {
+// 	next();
+// });
 
 // Cross Origin Resource Sharing
 app.use(cors());
